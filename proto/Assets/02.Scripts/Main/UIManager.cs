cf130f02d76f57c6c68fbf493ca78cd67e008b18
BearH0use <dndwn1125@gmail.com>
2020-03-06 20:45:57 +0900

    Bear 2nd

---
+++ proto/Assets/02.Scripts/Main/UIManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public void OnClickStartBtn(RectTransform rt)
+    {
+        Debug.Log("Scale X : " + rt.localScale.x.ToString());
+    }
+}
